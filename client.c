a76a19847e7240f90fec2481e797de2a5cf270af
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2025-05-24 17:54:08 +0900

    client exe args

---
+++ client.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char* argv[]) {
+    // argv[0]: program name
+    if (argc != 4)
+        return 1; // initialization error
+    const char* ip = argv[1];
+    const char* port = argv[2];
+    const char* username = argv[3];
+
+
+
+    return 0;
+}
